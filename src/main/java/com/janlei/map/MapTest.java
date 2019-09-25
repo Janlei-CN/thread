f0c4faea99acdf7f8d3b73758403b0ea4ab3c8a5
Janlei-CN <janlei4597@gmail.com>
2019-09-25 21:02:42 +0800

    test HashMap

---
+++ src/main/java/com/janlei/map/MapTest.java
@@ -0,0 +1,31 @@
+package com.janlei.map;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest {
+
+    public Map init(){
+        Map<String,Integer> map = Collections.synchronizedMap(new HashMap<String,Integer>());
+        return map;
+    }
+
+    public Map put(Map<String,Integer> map, String key, Integer value){
+        map.put(key, value);
+        return map;
+    }
+    
+    public void print(Map<String,Integer> map){
+        for (Map.Entry<String,Integer> entry:
+                map.entrySet()
+             ) {
+            System.out.println("key:"+entry.getKey()+"   value:"+entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
