0bcbd1dea69e776e147f8cfdd71c3f8db5c2cc1f
Janlei-CN <janlei4597@gmail.com>
2019-09-30 23:14:10 +0800

    udpate SynchronizedTest

--- src/main/java/com/janlei/thread/sync/SynchronizedTest.java
+++ src/main/java/com/janlei/thread/sync/SynchronizedTest.java
@@ -3,45 +3,45 @@ package com.janlei.thread.sync;
 /**
  * 多线程操作的原子性
  */
-public class SynchronizedTest implements Runnable{
+public class SynchronizedTest implements Runnable {
 
     /**
      * 使用volatile可以使各个线程的数据同步
      */
-    private volatile int count = 10;
+    private int count = 10;
 
     /**
      * 没有Synchronized
-     *      Thread-1-count = 8
-     *      Thread-2-count = 7
-     *      Thread-0-count = 9
-     *      Thread-3-count = 6
-     *      Thread-4-count = 5
-     *      循环次数：1
-     *
-     *      synchronized (SynchronizedTest.class) {
-     *             count--;
-     *         }
-     *          Thread-30-count = 9
-     *          Thread-31-count = 8
-     *          Thread-32-count = 7
-     *          Thread-33-count = 6
-     *          Thread-34-count = 5
-     *          循环次数：7
-     *
-     *   还是无法保证原子性
+     * Thread-1-count = 8
+     * Thread-2-count = 7
+     * Thread-0-count = 9
+     * Thread-3-count = 6
+     * Thread-4-count = 5
+     * 循环次数：1
+     * <p>
+     * synchronized (SynchronizedTest.class) {
+     * count--;
+     * }
+     * Thread-30-count = 9
+     * Thread-31-count = 8
+     * Thread-32-count = 7
+     * Thread-33-count = 6
+     * Thread-34-count = 5
+     * 循环次数：7
+     * <p>
+     * 还是无法保证原子性
      */
     @Override
-    public void run() {
+    public synchronized void run() {
 //        synchronized (SynchronizedTest.class) {
 //            count--;
 //        }
         count--;
-        System.out.println(Thread.currentThread().getName()+"-count = "+count);
+        System.out.println(Thread.currentThread().getName() + "-count = " + count);
     }
 
     public static void main(String[] args) {
-        int index=0;
+        int index = 0;
         for (int i = 0; i < 10; i++) {
             SynchronizedTest t = new SynchronizedTest();
             for (int j = 0; j < 5; j++) {
@@ -53,7 +53,7 @@ public class SynchronizedTest implements Runnable{
                 e.printStackTrace();
             }
             index++;
-            System.out.println("循环次数："+index);
+            System.out.println("循环次数：" + index);
         }
     }
 }
