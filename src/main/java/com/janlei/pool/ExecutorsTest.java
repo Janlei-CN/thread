113e90ca841085c3dce68fc0807025c5b68a65e3
Janlei-CN <janlei4597@gmail.com>
2019-10-23 13:35:35 +0800

    update ExecutorsTest

--- src/main/java/com/janlei/pool/ExecutorsTest.java
+++ src/main/java/com/janlei/pool/ExecutorsTest.java
@@ -15,18 +15,55 @@ public class ExecutorsTest {
      */
     public ExecutorService init(String name){
         switch (name) {
+
+            /**
+             * 创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。
+             */
             case "cached":
                 ex = Executors.newCachedThreadPool();
                 break;
+
+            /**
+             * 创建一个指定工作线程数量的线程池。每当提交一个任务就创建一个工作线程，如果工作线程数量达到线程池初始的最大数，则将提交的任务存入到池队列中
+             */
             case "fixed":
                 ex = Executors.newFixedThreadPool(16);
                 break;
+
+            /**
+             * 创建一个单线程化的Executor，即只创建唯一的工作者线程来执行任务，
+             * 它只会用唯一的工作线程来执行任务，保证所有任务按照指定顺序(FIFO, LIFO,优先级)执行。
+             * 如果这个线程异常结束，会有另一个取代它，保证顺序执行。
+             * 单工作线程最大的特点是可保证顺序地执行各个任务，并且在任意给定的时间不会有多个线程是活动的。
+             */
             case "single":
                 ex = Executors.newSingleThreadExecutor();
                 break;
+
+            /**
+             * newScheduleThreadPool
+             * 创建一个定长的线程池，而且支持定时的以及周期性的任务执行，支持定时及周期性任务执行。
+             */
             default:
                 ex = Executors.newScheduledThreadPool(16);
         }
         return ex;
+    }
+
+    public void use(){
+        Thread thread = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                System.out.print(i+" ");
+            }
+        });
+        ex.submit(thread);
+        //ex.execute(thread);
+    }
+
+    public static void main(String[] args) {
+        ExecutorsTest test = new ExecutorsTest();
+        test.init("cached");
+        test.use();
+
     }
 }
